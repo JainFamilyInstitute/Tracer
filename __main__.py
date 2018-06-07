a88aa9237d3dcfa1e37aefa3ccf9072022a94747
jh4mit <jhodgesiv@gmail.com>
2018-06-07 17:24:53 -0400

    Create DEBT_base

--- __main__.py
+++ __main__.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Run the DEBT version of the model.')
     parser.add_argument('gamma', metavar='GAMMA', type=float, help='Gamma')
-    parser.add_argument('n_sim', metavar='N_SIM', type=float, help='Number of MC loops.')
+    parser.add_argument('n_sim', metavar='N_SIM', type=int, help='Number of MC loops.')
     args = parser.parse_args()
     main(version=VERSION, gamma=args.gamma, n_sim=args.n_sim)
