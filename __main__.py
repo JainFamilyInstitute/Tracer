f703edf1044db7a3a650f80a273131435ad380b2
Sidhya26 <40043169+Sidhya26@users.noreply.github.com>
2018-10-01 16:10:30 -0400

    Updated discrete Kimball CRRAs

--- __main__.py
+++ __main__.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
         description='Run the ISA version of the model.')
 
     parser.add_argument('n_sim', metavar='N_SIM', type=int, help='Number of MC loops.')
-    parser.add_argument('-g','--gamma', metavar='GAMMA', type=str, help='Gamma (float; if not specified, uses all of [1.0, 2.0, 3.0, 4.0])',default='ALL')
+    parser.add_argument('-g','--gamma', metavar='GAMMA', type=str, help='Gamma (float; if not specified, uses all of [1.4, 2.2, 3.5, 4.2, 6.7])',default='ALL')
 
     args = parser.parse_args()
     gamma = args.gamma
     if gamma.upper() == "ALL":
-        gamma = [1.0, 2.0, 3.0, 4.0]
+        gamma = [1.4, 2.2, 3.5, 4.2, 6.7]
     else:
         gamma = float(gamma)
 
