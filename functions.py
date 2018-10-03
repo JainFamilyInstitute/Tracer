df4b08d97d992f27f5a0b2c18ff4cbc7fdf572e5
jh4mit <jhodgesiv@gmail.com>
2018-10-03 17:31:21 -0400

    delete .idea

--- functions.py
+++ functions.py
@@ -62,6 +62,7 @@ def adj_income_process(income, sigma_perm, sigma_tran, INIT_DEBT, P_BAR, N_SIM):
     ret_income_vec = ret_frac[AltDeg] * np.tile(inc_with_inc_risk[:, -1], (END_AGE - RETIRE_AGE, 1)).T
     inc_with_inc_risk = np.append(inc_with_inc_risk, ret_income_vec, axis=1)
 
+
     # # unemployment risk
     # Y_list = []
     # for unemp_flag in [True, False]:
