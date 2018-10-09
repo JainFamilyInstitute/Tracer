ea373825a7c8a328a468b3598ab9938ea66c524e
Sidhya26 <40043169+Sidhya26@users.noreply.github.com>
2018-10-09 10:36:08 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
         description='Run the DEBT version of the model.')
 
     parser.add_argument('n_sim', metavar='N_SIM', type=int, help='Number of MC loops.')
-    parser.add_argument('-g','--gamma', metavar='GAMMA', type=str, help='Gamma (float; if not specified, uses all of [1.4, 2.2, 3.5, 4.2, 6.7])',default='ALL')
+    parser.add_argument('-g','--gamma', metavar='GAMMA', type=str, help='Gamma (float; if not specified, uses all of [1, 2, 3, 4])',default='ALL')
 
     args = parser.parse_args()
     gamma = args.gamma
     if gamma.upper() == "ALL":
-        gamma = [1.4, 2.2, 3.5, 4.2, 6.7]
+        gamma = [1, 2, 3, 4]
     else:
         gamma = float(gamma)
 
