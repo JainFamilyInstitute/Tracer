e2e784f914e4fdd8a41e314164cfe0d60ad202f8
Sidhya26 <40043169+Sidhya26@users.noreply.github.com>
2018-10-09 12:07:31 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,20 +2,20 @@
 import argparse
 from main import main
 
-VERSION = 'DEBT'
+VERSION = 'ISA_MC'
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Run the DEBT version of the model.')
+        description='Run the ISA version of the model.')
 
     parser.add_argument('n_sim', metavar='N_SIM', type=int, help='Number of MC loops.')
-    parser.add_argument('-g','--gamma', metavar='GAMMA', type=str, help='Gamma (float; if not specified, uses all of [1, 2, 3, 4])',default='ALL')
+    parser.add_argument('-g','--gamma', metavar='GAMMA', type=str, help='Gamma (float; if not specified, uses all of [1.0, 2.0, 3.0, 4.0])',default='ALL')
 
     args = parser.parse_args()
     gamma = args.gamma
     if gamma.upper() == "ALL":
-        gamma = [1, 2, 3, 4]
+        gamma = [1.0, 2.0, 3.0, 4.0]
     else:
         gamma = float(gamma)
 
