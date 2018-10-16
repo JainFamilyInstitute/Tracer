a2516c8a5916770259992fc52ae22b2294f8aa6c
Quintus <quancheng.zhang@rutgers.edu>
2018-10-16 12:38:25 -0400

    output utilities

--- test.py
+++ test.py
@@ -96,7 +96,56 @@ sigma_tran = std.loc['sigma_transitory', 'Labor Income Only'][education_level[Al
 # run_model(param_pair, income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, 10000)
 
 
+# # debt
+# debt_dict = {
+#     805: 5000,
+#     1610: 10000,
+#     2416: 15000,
+#     3221: 20000,
+#     4027: 25000,
+#     4832: 30000,
+#     5638: 35000,
+#     6443: 40000,
+#     7249: 45000,
+#     8054: 50000,
+#     8859: 55000,
+#     9665: 60000,
+#     10470: 65000,
+#     11276: 70000
+# }
+#
+#
+# def run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, n_sim):
+#
+#     start = time.time()
+#
+#     cfunc_fps = glob.glob(os.path.join(base_path, 'data', 'c_DEBT*'))
+#
+#     for fp in cfunc_fps:
+#         c_func_df = pd.read_excel(fp)
+#         gamma = float(fp.split('.')[-3][-1])
+#         ppt_bar = float(fp.split('/')[-1].split('_')[2])
+#         principal = debt_dict[int(ppt_bar)]
+#
+#         adj_income = adj_income_process(income_bf_ret, sigma_perm, sigma_tran, principal, ppt_bar, n_sim)
+#
+#         c_proc, _ = generate_consumption_process(adj_income, c_func_df, adj_income.shape[0])
+#         prob = surv_prob.loc[START_AGE:END_AGE, 'CSP'].cumprod().values
+#         ce = cal_ce_agent(prob, c_proc, gamma)
+#
+#         print(f'########## ppt_bar: {ppt_bar} | principal: {principal} | Gamma: {gamma} | Exp_Frac: {gamma_exp_frac[gamma]} ##########')
+#         print(f"------ {time.time() - start} seconds ------")
+#
+#         col_names = [str(x + START_AGE) for x in range(END_AGE - START_AGE + 1)]
+#         df = pd.DataFrame(adj_income, columns=col_names)
+#         df['CE'] = ce
+#         df.to_csv(os.path.join(base_path, 'results', f'DEBT_inc_CEs_pptbar{ppt_bar}_Gamma{gamma}.csv'))
+#
+# run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, 10000)
+
+
 # debt
+# output utility
 debt_dict = {
     805: 5000,
     1610: 10000,
@@ -131,16 +180,16 @@ def run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, n_sim
 
         c_proc, _ = generate_consumption_process(adj_income, c_func_df, adj_income.shape[0])
         prob = surv_prob.loc[START_AGE:END_AGE, 'CSP'].cumprod().values
-        ce = cal_ce_agent(prob, c_proc, gamma)
+        ce, util = cal_ce_agent(prob, c_proc, gamma)
 
         print(f'########## ppt_bar: {ppt_bar} | principal: {principal} | Gamma: {gamma} | Exp_Frac: {gamma_exp_frac[gamma]} ##########')
         print(f"------ {time.time() - start} seconds ------")
 
         col_names = [str(x + START_AGE) for x in range(END_AGE - START_AGE + 1)]
-        df = pd.DataFrame(adj_income, columns=col_names)
+        df = pd.DataFrame(util, columns=['Utility'])
         df['CE'] = ce
-        df.to_csv(os.path.join(base_path, 'results', f'DEBT_inc_CEs_pptbar{ppt_bar}_Gamma{gamma}.csv'))
+        df.to_csv(os.path.join(base_path, 'results', f'DEBT_Utils_CEs_pptbar{ppt_bar}_Gamma{gamma}.csv'))
+
 
 run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, 10000)
-
 
