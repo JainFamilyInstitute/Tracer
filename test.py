83bbdc5c3ef36ecdf4d8d3c6038c54fccdd603e4
Quintus <quancheng.zhang@rutgers.edu>
2018-11-05 13:57:26 -0500

    generate quartile retuslts for ISA-Purdue

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ from constants import *
 import multiprocessing as mp
 import itertools
 from main import run_model
+import glob
 
 ###########################################################################
 #                      Setup - file path & raw data                       #
@@ -34,8 +35,70 @@ income_bf_ret = cal_income(age_coeff)
 sigma_perm = std.loc['sigma_permanent', 'Labor Income Only'][education_level[AltDeg]]
 sigma_tran = std.loc['sigma_transitory', 'Labor Income Only'][education_level[AltDeg]]
 
-params = 33934
-n_sim = 10000
-gamma = 2.0
-run_model(params, income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, n_sim, gamma)
+#
+# params = 33934
+# n_sim = 10000
+# gamma = 2.0
+# run_model(params, income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, n_sim, gamma)
 
+
+# for Quartile IPCU
+def run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, n_sim):
+    term = 10
+
+    start = time.time()
+
+    cfunc_fps = glob.glob(os.path.join(base_path, 'data', 'c_ISA_*'))
+
+    op = []
+    idx_names = []
+    for fp in cfunc_fps:
+        c_func_df = pd.read_excel(fp)
+        principal = float(fp.split('/')[-1].split('_')[3][1:-1])
+        gamma = float(fp.split('/')[-1].split('_')[4])
+
+        iterables = [[1, 2, 3, 4],
+                     ['Income', 'Payment', 'Consumption', 'Utility']]
+        index = pd.MultiIndex.from_product(iterables, names=['Quartile', 'Category'])
+        column = np.arange(22, 101)
+        ipcu_df = pd.DataFrame(index=index, columns=column)
+
+        idx_names.append(str(principal)+'_'+str(gamma))
+
+        adj_income, payment, income = adj_income_process(income_bf_ret, sigma_perm, sigma_tran, principal, n_sim)
+
+        single_op = []
+        for l, r, i in zip(np.arange(0, 1, 0.25), np.arange(0, 1, 0.25) + 0.25, np.arange(4) + 1):
+            discount_array = DELTA**np.arange(adj_income.shape[1])
+            discount_Y = np.multiply(adj_income, discount_array)
+            npvs = np.sum(discount_Y, axis=1)
+            allowed_rows = np.where(np.logical_and(npvs > np.quantile(npvs, l), npvs < np.quantile(npvs, r)))
+            cur_intvl = adj_income[allowed_rows]
+
+            # op: Income, Payment, Adjusted Income
+            q_ave_inc = np.mean(income[allowed_rows], axis=0)
+            q_ave_pmt = np.mean(payment[allowed_rows], axis=0)
+
+            c_proc, _ = generate_consumption_process(cur_intvl, c_func_df, cur_intvl.shape[0])
+            # op: Consumption
+            q_ave_c = np.mean(c_proc, axis=0)
+
+            prob = surv_prob.loc[START_AGE:END_AGE, 'CSP'].cumprod().values
+            c_ce, util = cal_certainty_equi(prob, c_proc, gamma)
+            # op: Utility
+            q_ave_util = np.mean(util, axis=0)
+
+            single_op.append(c_ce)
+            ipcu_df.loc[i] = np.array([q_ave_inc, q_ave_pmt, q_ave_c, q_ave_util])
+
+        print(
+            f'########## Term: {term} | Principal: {principal:.2f} | Gamma: {gamma} | Exp_Frac: {gamma_exp_frac[gamma]} | CE: {c_ce:.2f} ##########')
+        print(f"------ {time.time() - start} seconds ------")
+        op.append(single_op)
+
+        ipcu_df.to_csv(os.path.join(base_path, 'results', f'ISA-Purdue_quantile_IPCU_{principal}_gamma_{gamma}.csv'))
+    df = pd.DataFrame(op, index=idx_names)
+    df.to_csv(os.path.join(base_path, 'results', 'ISA-Purdue_quantile_CEs.csv'))
+
+
+run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, 10000)
