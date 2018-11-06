e73bfa4624eb7f8ac94012771470e9a7dbe23d82
Quintus <quancheng.zhang@rutgers.edu>
2018-11-06 12:23:21 -0500

    use fixed income file

--- test.py
+++ test.py
@@ -40,65 +40,65 @@ debt_dict = {
 }
 
 
-def run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, n_sim):
-
-    start = time.time()
-
-    cfunc_fps = glob.glob(os.path.join(base_path, 'data', 'c_Debt*'))
-
-    op = []
-    idx_names = []
-    for fp in cfunc_fps:
-        c_func_df = pd.read_excel(fp)
-        gamma = float(fp.split('.')[-3][-1])
-        ppt_bar = float(fp.split('/')[-1].split('_')[2])
-
-        iterables = [[1, 2, 3, 4],
-                     ['Income', 'Payment', 'Consumption', 'Utility']]
-        index = pd.MultiIndex.from_product(iterables, names=['Quartile', 'Category'])
-        column = np.arange(22, 101)
-        ipcu_df = pd.DataFrame(index=index, columns=column)
-
-        if int(ppt_bar) in debt_dict:
-            principal = debt_dict[int(ppt_bar)]
-            idx_names.append(str(ppt_bar) + '_' + str(gamma))
-
-            adj_income, income, payments = adj_income_process(income_bf_ret, sigma_perm, sigma_tran,
-                                                              principal, ppt_bar, n_sim, path=base_path)
-
-            single_op = []
-            for l, r, i in zip(np.arange(0, 1, 0.25), np.arange(0, 1, 0.25) + 0.25, np.arange(4) + 1):
-                discount_array = DELTA**np.arange(income.shape[1])
-                discount_Y = np.multiply(income, discount_array)
-                npvs = np.sum(discount_Y, axis=1)
-                allowed_rows = np.where(np.logical_and(npvs >= np.quantile(npvs, l), npvs < np.quantile(npvs, r)))
-                cur_intvl = income[allowed_rows]
-
-                # op: Income, Payment, Adjusted Income
-                q_ave_inc = np.mean(income[allowed_rows], axis=0)
-                q_ave_pmt = np.mean(payments[allowed_rows], axis=0)
-
-                c_proc, _ = generate_consumption_process(cur_intvl, c_func_df, cur_intvl.shape[0])
-                # op: Consumption
-                q_ave_c = np.mean(c_proc, axis=0)
-
-                prob = surv_prob.loc[START_AGE:END_AGE, 'CSP'].cumprod().values
-                c_ce, _, util = cal_certainty_equi(prob, c_proc, gamma)
-                # op: Utility
-                q_ave_util = np.mean(util, axis=0)
-                single_op.append(c_ce)
-                ipcu_df.loc[i] = np.array([q_ave_inc, q_ave_pmt, q_ave_c, q_ave_util])
-
-            print(f'########## ppt_bar: {ppt_bar} | principal: {principal} | Gamma: {gamma} | Exp_Frac: {gamma_exp_frac[gamma]} | CE: {c_ce:.2f} ##########')
-            print(f"------ {time.time() - start} seconds ------")
-            op.append(single_op)
-
-            ipcu_df.to_csv(os.path.join(base_path, 'results', f'debt_quantile_IPCU_{ppt_bar}_gamma_{gamma}.csv'))
-    df = pd.DataFrame(op, index=idx_names)
-    df.to_csv(os.path.join(base_path, 'results', 'debt_quantile_CEs.csv'))
-
-
-run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, 10000)
+# def run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, n_sim):
+#
+#     start = time.time()
+#
+#     cfunc_fps = glob.glob(os.path.join(base_path, 'data', 'c_Debt*'))
+#
+#     op = []
+#     idx_names = []
+#     for fp in cfunc_fps:
+#         c_func_df = pd.read_excel(fp)
+#         gamma = float(fp.split('.')[-3][-1])
+#         ppt_bar = float(fp.split('/')[-1].split('_')[2])
+#
+#         iterables = [[1, 2, 3, 4],
+#                      ['Income', 'Payment', 'Consumption', 'Utility']]
+#         index = pd.MultiIndex.from_product(iterables, names=['Quartile', 'Category'])
+#         column = np.arange(22, 101)
+#         ipcu_df = pd.DataFrame(index=index, columns=column)
+#
+#         if int(ppt_bar) in debt_dict:
+#             principal = debt_dict[int(ppt_bar)]
+#             idx_names.append(str(ppt_bar) + '_' + str(gamma))
+#
+#             adj_income, income, payments = adj_income_process(income_bf_ret, sigma_perm, sigma_tran,
+#                                                               principal, ppt_bar, n_sim, path=base_path)
+#
+#             single_op = []
+#             for l, r, i in zip(np.arange(0, 1, 0.25), np.arange(0, 1, 0.25) + 0.25, np.arange(4) + 1):
+#                 discount_array = DELTA**np.arange(income.shape[1])
+#                 discount_Y = np.multiply(income, discount_array)
+#                 npvs = np.sum(discount_Y, axis=1)
+#                 allowed_rows = np.where(np.logical_and(npvs >= np.quantile(npvs, l), npvs < np.quantile(npvs, r)))
+#                 cur_intvl = income[allowed_rows]
+#
+#                 # op: Income, Payment, Adjusted Income
+#                 q_ave_inc = np.mean(income[allowed_rows], axis=0)
+#                 q_ave_pmt = np.mean(payments[allowed_rows], axis=0)
+#
+#                 c_proc, _ = generate_consumption_process(cur_intvl, c_func_df, cur_intvl.shape[0])
+#                 # op: Consumption
+#                 q_ave_c = np.mean(c_proc, axis=0)
+#
+#                 prob = surv_prob.loc[START_AGE:END_AGE, 'CSP'].cumprod().values
+#                 c_ce, _, util = cal_certainty_equi(prob, c_proc, gamma)
+#                 # op: Utility
+#                 q_ave_util = np.mean(util, axis=0)
+#                 single_op.append(c_ce)
+#                 ipcu_df.loc[i] = np.array([q_ave_inc, q_ave_pmt, q_ave_c, q_ave_util])
+#
+#             print(f'########## ppt_bar: {ppt_bar} | principal: {principal} | Gamma: {gamma} | Exp_Frac: {gamma_exp_frac[gamma]} | CE: {c_ce:.2f} ##########')
+#             print(f"------ {time.time() - start} seconds ------")
+#             op.append(single_op)
+#
+#             ipcu_df.to_csv(os.path.join(base_path, 'results', f'debt_quantile_IPCU_{ppt_bar}_gamma_{gamma}.csv'))
+#     df = pd.DataFrame(op, index=idx_names)
+#     df.to_csv(os.path.join(base_path, 'results', 'debt_quantile_CEs.csv'))
+#
+#
+# run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, 10000)
 
 
 # # hsg for quantile
@@ -160,60 +160,60 @@ run_model(income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, 10000)
 # run_model(param_pair, income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, 10000)
 
 
-# # cg for quantile
-# def run_model(param_pair, income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, n_sim):
-#     principal = param_pair[0]
-#     ppt_bar = param_pair[1]
-#
-#     start = time.time()
-#
-#     income = adj_income_process(income_bf_ret, sigma_perm, sigma_tran, principal, ppt_bar, n_sim)
-#
-#     ###########################################################################
-#     #                    DP - read consumption functions                      #
-#     ###########################################################################
-#     cfunc_fps = glob.glob(os.path.join(base_path, 'data', 'c_CG*'))
-#
-#     op = []
-#     for fp in cfunc_fps:
-#         c_func_df = pd.read_excel(fp)
-#         gamma = float(fp.split('.')[-3][-1])
-#
-#         iterables = [[1, 2, 3, 4],
-#                      ['Income', 'Consumption', 'Utility']]
-#         index = pd.MultiIndex.from_product(iterables, names=['Quartile', 'Category'])
-#         column = np.arange(22, 101)
-#         ipcu_df = pd.DataFrame(index=index, columns=column)
-#
-#         single_op = []
-#         for l, r, i in zip(np.arange(0, 1, 0.25), np.arange(0, 1, 0.25) + 0.25, np.arange(4) + 1):
-#             discount_array = DELTA ** np.arange(income.shape[1])
-#             discount_Y = np.multiply(income, discount_array)
-#             npvs = np.sum(discount_Y, axis=1)
-#             allowed_rows = np.where(np.logical_and(npvs >= np.quantile(npvs, l), npvs < np.quantile(npvs, r)))
-#             cur_intvl = income[allowed_rows]
-#
-#             # op: Income, Payment, Adjusted Income
-#             q_ave_inc = np.mean(income[allowed_rows], axis=0)
-#
-#             c_proc, _ = generate_consumption_process(cur_intvl, c_func_df, cur_intvl.shape[0])
-#             # op: Consumption
-#             q_ave_c = np.mean(c_proc, axis=0)
-#
-#             prob = surv_prob.loc[START_AGE:END_AGE, 'CSP'].cumprod().values
-#             c_ce, _, util = cal_certainty_equi(prob, c_proc, gamma)
-#             # op: Utility
-#             q_ave_util = np.mean(util, axis=0)
-#             single_op.append(c_ce)
-#             ipcu_df.loc[i] = np.array([q_ave_inc, q_ave_c, q_ave_util])
-#
-#         print(f'########## Gamma: {ppt_bar} | Gamma: {gamma} | Exp_Frac: {gamma_exp_frac[gamma]} | CE: {c_ce:.2f} ##########')
-#         print(f"------ {time.time() - start} seconds ------")
-#         op.append(single_op)
-#         ipcu_df.to_csv(os.path.join(base_path, 'results', f'cg_quantile_IPCU_gamma_{gamma}.csv'))
-#     df = pd.DataFrame(op)
-#     df.to_csv(os.path.join(base_path, 'results', 'cg_quantile_CEs.csv'))
-#
-#
-# param_pair = [None, None]
-# run_model(param_pair, income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, 10000)
+# cg for quantile
+def run_model(param_pair, income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, n_sim):
+    principal = param_pair[0]
+    ppt_bar = param_pair[1]
+
+    start = time.time()
+
+    income = adj_income_process(income_bf_ret, sigma_perm, sigma_tran, principal, ppt_bar, n_sim, path=base_path)
+
+    ###########################################################################
+    #                    DP - read consumption functions                      #
+    ###########################################################################
+    cfunc_fps = glob.glob(os.path.join(base_path, 'data', 'c_CG*'))
+
+    op = []
+    for fp in cfunc_fps:
+        c_func_df = pd.read_excel(fp)
+        gamma = float(fp.split('.')[-3][-1])
+
+        iterables = [[1, 2, 3, 4],
+                     ['Income', 'Consumption', 'Utility']]
+        index = pd.MultiIndex.from_product(iterables, names=['Quartile', 'Category'])
+        column = np.arange(22, 101)
+        ipcu_df = pd.DataFrame(index=index, columns=column)
+
+        single_op = []
+        for l, r, i in zip(np.arange(0, 1, 0.25), np.arange(0, 1, 0.25) + 0.25, np.arange(4) + 1):
+            discount_array = DELTA ** np.arange(income.shape[1])
+            discount_Y = np.multiply(income, discount_array)
+            npvs = np.sum(discount_Y, axis=1)
+            allowed_rows = np.where(np.logical_and(npvs >= np.quantile(npvs, l), npvs < np.quantile(npvs, r)))
+            cur_intvl = income[allowed_rows]
+
+            # op: Income, Payment, Adjusted Income
+            q_ave_inc = np.mean(income[allowed_rows], axis=0)
+
+            c_proc, _ = generate_consumption_process(cur_intvl, c_func_df, cur_intvl.shape[0])
+            # op: Consumption
+            q_ave_c = np.mean(c_proc, axis=0)
+
+            prob = surv_prob.loc[START_AGE:END_AGE, 'CSP'].cumprod().values
+            c_ce, _, util = cal_certainty_equi(prob, c_proc, gamma)
+            # op: Utility
+            q_ave_util = np.mean(util, axis=0)
+            single_op.append(c_ce)
+            ipcu_df.loc[i] = np.array([q_ave_inc, q_ave_c, q_ave_util])
+
+        print(f'########## Gamma: {ppt_bar} | Gamma: {gamma} | Exp_Frac: {gamma_exp_frac[gamma]} | CE: {c_ce:.2f} ##########')
+        print(f"------ {time.time() - start} seconds ------")
+        op.append(single_op)
+        ipcu_df.to_csv(os.path.join(base_path, 'results', f'cg_quantile_IPCU_gamma_{gamma}.csv'))
+    df = pd.DataFrame(op)
+    df.to_csv(os.path.join(base_path, 'results', 'cg_quantile_CEs.csv'))
+
+
+param_pair = [None, None]
+run_model(param_pair, income_bf_ret, sigma_perm, sigma_tran, surv_prob, base_path, 10000)
