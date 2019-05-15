0f2b1192f0a8887ceeca68f42bf2255d034ce31f
Sidhya26 <40043169+Sidhya26@users.noreply.github.com>
2019-05-15 17:00:07 -0400

    data file name corrected

--- main.py
+++ main.py
@@ -61,8 +61,8 @@ def main(version, n_sim, gamma):
     ###########################################################################
     #                      Setup - file path & raw data                       #
     ###########################################################################
-    income_fn = 'age_coefficients_and_var.xlsx'
-    surviv_fn = 'Conditional Survival Prob Feb 16.xlsx'
+    income_fn = 'age_coefficients_and_var_May152019.xlsx'
+    surviv_fn = 'Conditional Survival Prob_May152019.xlsx'
     isa_fn = 'Loop on term and rho.xlsx'
     base_path = os.path.dirname(__file__)
     income_fp = os.path.join(base_path, 'data', income_fn)
