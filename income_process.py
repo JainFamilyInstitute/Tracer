9527ec58f7b0d3a21b9cb815ff282c894d418dfb
jh4mit <jh4@jainfamilyinstitute.org>
2020-01-16 19:36:48 -0500

    rework income, file reading

---
+++ income_process.py
@@ -0,0 +1,66 @@
+from constants import unemp_frac, education_level, start_ages, RETIRE_AGE, MU, END_AGE, unemp_rate, N_C, ret_frac
+from scipy.stats import bernoulli
+
+def cal_income(coeffs, alt_deg):
+    coeff_this_group = coeffs.loc[education_level[alt_deg]]
+    a  = coeff_this_group['a']
+    b1 = coeff_this_group['b1']
+    b2 = coeff_this_group['b2']
+    b3 = coeff_this_group['b3']
+
+    ages = np.arange(start_ages[alt_deg], RETIRE_AGE+1)      # 22 to 65
+
+    income = (a + b1 * ages + b2 * ages**2 + b3 * ages**3)  # 0:43, 22:65
+    return income
+
+def base_income_process(income, sigma_perm, sigma_tran, n_sim, alt_deg):
+    # generate random walk and normal r.v.
+    np.random.seed(0)
+    rn_perm = np.random.normal(MU, sigma_perm, (n_sim, RETIRE_AGE - start_ages[alt_deg] + 1))
+    rand_walk = np.cumsum(rn_perm, axis=1)
+    np.random.seed(1)
+    rn_tran = np.random.normal(MU, sigma_tran, (n_sim, RETIRE_AGE - start_ages[alt_deg] + 1))
+    inc_with_inc_risk = np.multiply(np.exp(rand_walk) * np.exp(rn_tran), income)
+
+    # - retirement
+    ret_income_vec = ret_frac[alt_deg] * np.tile(inc_with_inc_risk[:, -1], (END_AGE - RETIRE_AGE, 1)).T
+    inc_with_inc_risk = np.append(inc_with_inc_risk, ret_income_vec, axis=1)
+
+    # unemployment risk
+    # generate bernoulli random variable
+    np.random.seed(seed=2)
+    p = 1 - unemp_rate[alt_deg]
+    r = bernoulli.rvs(p, size=(RETIRE_AGE - start_ages[alt_deg] + 1, n_sim)).astype(float)
+    r[r == 0] = unemp_frac[alt_deg]
+    ones = np.ones((END_AGE - RETIRE_AGE, n_sim))
+    bern = np.append(r, ones, axis=0)
+    Y = np.multiply(inc_with_inc_risk, bern.T)
+    return Y
+
+def adj_income_process_isa(Y, term, rho):
+    # adjust income with ISA
+    Y[:, :term] *= rho
+    return Y
+
+
+def adj_income_process_loan(Y, alt_deg, init_debt, p_bar):
+     # adjust income with debt repayment
+    D = np.zeros(Y.shape)
+    D[:, 0] = init_debt
+    P = np.zeros(Y.shape)
+
+    for t in range(END_AGE - start_ages[alt_deg]):
+        cond1 = np.logical_and(Y[:, t] >= 2 * p_bar, D[:, t] >= p_bar)
+        cond2 = np.logical_and(Y[:, t] >= 2 * D[:, t], D[:, t] < p_bar)
+        cond3 = np.logical_and(Y[:, t] < 2 * p_bar, D[:, t] >= p_bar)
+        cond4 = np.logical_and(Y[:, t] < 2 * D[:, t], D[:, t] < p_bar)
+
+        P[cond1, t] = p_bar
+        P[cond2, t] = D[cond2, t] * (1 + rate)
+        P[cond3, t] = Y[cond3, t] / 2
+        P[cond4, t] = Y[cond4, t] / 2
+
+        D[:, t + 1] = D[:, t] * (1 + rate) - P[:, t]
+        D[cond2, t + 1] = 0
+    adj_Y = Y - P
+    return adj_Y
---
+++ input_readers.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def read_ids(id_fp, alt_deg):
+    # ids for specified alt_deg
+    ids = pd.read_excel(id_fp)
+    ids = ids[ids['AltDeg'] == education_level[alt_deg]]
+    return ids
+
+def read_age_coeff(income_fp, mortal_fp):
+    # age coefficients
+    age_coeff = pd.read_excel(income_fp, sheet_name='Coefficients', index_col=0)
+    return age_coeff
+
+def read_variance(income_fp):
+    # decomposed variance
+    std = pd.read_excel(income_fp, sheet_name='Variance', header=[1, 2], index_col=0)
+    std.reset_index(inplace=True)
+    std.drop(std.columns[0], axis=1, inplace=True)
+    std.drop([1, 3], inplace=True)
+    std.index = pd.CategoricalIndex(['sigma_permanent', 'sigma_transitory'])
+    return std
+
+def read_survival(mortal_fp):
+    # conditional survival probabilities
+    cond_prob = pd.read_excel(mortal_fp)
+    cond_prob.set_index('AGE', inplace=True)
+    return cond_prob
