31bb60773dfc1e697143dc619767a7a16d2476fd
jh4mit <jh4@jainfamilyinstitute.org>
2020-01-20 18:49:33 -0500

    get rid of filenames, argparse leftovers in __main__

--- __main__.py
+++ __main__.py
@@ -3,15 +3,5 @@ from main import main
 
 if __name__ == "__main__":
     alt_degs = [2,3,4]
-    id_fn = "Mixture_IDs.xls"
     gammas = [1.0, 2.0, 3.0, 4.0]
-    
-    try:
-        assert os.path.isfile(id_fn)
-        id_fp = id_fn
-    except:
-        base_path = os.path.dirname(__file__)
-        id_fp = os.path.join(base_path, 'data', id_fn)
-        assert os.path.isfile(id_fp)
-
-    main(id_fp=id_fp, alt_degs=alt_degs, gammas=gammas)
+    main(alt_degs=alt_degs, gammas=gammas)
