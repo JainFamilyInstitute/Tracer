8da63bd037350608027723d1232da597b459d50b
jh4mit <jhodgesiv@gmail.com>
2020-01-22 18:21:08 -0500

    rename income_fn -> coeff_fn; consolidate output fn handling; doc for constants

--- constants.py
+++ constants.py
@@ -1,17 +1,18 @@
 ###########################################################################
 #                            Set Constants                                #
 ###########################################################################
-END_AGE = 100             
+N_SIM = 1e4                     # number of simulated individuals (per equivalence class)
+END_AGE = 100                   # last possible consumption age
 RETIRE_AGE = 65                 # retirement age
-N_W = 501
+N_W = 501                       # number of values in wealth grid
 LOWER_BOUND_W = 1               # lower bound of wealth
 UPPER_BOUND_W = 15000000        # upper bound of wealth
-N_C = 1501
-LOWER_BOUND_C = 0
+N_C = 1501                      # number of values in consumption grid
+LOWER_BOUND_C = 0               # lowest possible consumption value
 R = 0.02                        # risk-free rate
 DELTA = 0.99                    # discount factor
 MU = 0                          # expectation of income shocks
-INIT_WEALTH = 0                 #
+INIT_WEALTH = 0                 # initial wealth value
 ISA_POVERTY_MULTIPLIER = 2.0    # poverty multiplier for ISA lower threshold
 IDR_POVERTY_MULTIPLIER = 1.5    # poverty multiplier for IDR
 POVERTY_LEVEL = 10830           # poverty level
@@ -20,13 +21,15 @@ NOMINAL_CAP_MULTIPLIER = 2.5    # nominal cap multiplier for ISA
 run_dp = True
 
 # file names
-income_fn = 'age_coefficients_and_var.xlsx'
+coeff_fn = 'age_coefficients_and_var.xlsx'
 surviv_fn = 'Conditional Survival Prob.xlsx'
 isa_loop_fn = 'Loop on term and rho.xlsx'
 debt_loop_fn = 'Loop on Principal for Loan.xlsx'
 id_fn = "Mixture_IDS.xls"
 ce_fn = 'ce.xlsx'
+income_output_fn = 'base_income_'
 
+# age at which each education level starts consuming
 start_ages = {
     2: 18,
     3: 20,
